33ab2225f9f2690b024ffea813dda0b2e8b018f0
TECOA136_Aditya_ Deo <ahdeo2015@gmail.com>
2021-09-21 12:21:08 +0530

    create express app

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sql = require('sql');
+
+
+
+const app = express();
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server running on port 3000");
+});
