f7e83838c0c91038fd0a67594779d576a88dfaab
TECOA136_Aditya_ Deo <ahdeo2015@gmail.com>
2021-09-21 14:33:01 +0530

    experiment with node mysql2

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const mysql2 = require('mysql2');
 const app = express();
 
-var connection = mysql.createConnection({
+var connection = mysql2.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : 'mysql@123',
-    database : 'HotelManagement'
+    database : 'hotelmanagement'
   });
 
 
+  connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as id ' + connection.threadId);
+  });
 
 
+  connection.query(
+      'SELECT  * FROM `example`',
+      function(err,results,fields){
+        console.log(results); // results contains rows returned by server
+        // console.log(fields); // fields contains extra meta data about results, if available
+      }
 
+  )
 
 
 
