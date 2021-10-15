5bac904d75a38b3882b55924d57c0550d9cefc3f
Tejas Borde <tejas.borde@kpit.com>
2021-10-15 11:26:51 +0530

    commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const path = require("path");
 
 connectDB();
 
-app.use(express.static("client/build"));
+app.use(express.static("build"));
 
 app.use(cookieParser("secretcode"));
 app.use(
@@ -56,7 +56,7 @@ app.use(require("./routes/Records"));
 app.use(require("./routes/Rooms"));
 app.use(require("./routes/tempRoutes"));
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 app.listen(process.env.PORT || 8080, (err, res) => {
   if (err) {
